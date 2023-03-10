f0c3f27e617dc7bacf0ca13e65caca7bbc2c4342
Maria Eduarda Sales de Castro <castro.madusales@gmail.com>
2023-03-10 12:20:04 -0300

    add codes swiper JS slider

--- Hotel-arquivos/js/main.js
+++ Hotel-arquivos/js/main.js
@@ -3,6 +3,17 @@
 // Nav hide
 
 // Swiper Slider
+var swiper = new Swiper(".mySwiper", {
+    direction: "vertical",
+    loop: true,
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+    autoplay: {
+        delay: 3500,
+    },
+});
 
 // counter design
 
